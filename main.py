96c3d595a77d6820dd5597294aa9ca72a73e3ade
ezschneider <ezequielschneiderr@gmail.com>
2022-05-27 12:18:45 -0300

    rota LabGrad com requisição

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from distutils.log import debug
 from flask import Flask, render_template
+from distutils.log import debug, info
+
+import requests
 
 app = Flask(__name__)
 
@@ -8,6 +10,12 @@ app = Flask(__name__)
 def homepage():
     nome = "Ez"
     return render_template("homepage.html", nome=nome)
+
+# Retorna o json da API do LabGrad
+@app.route("/labgrad")
+def labgradpage():
+    req = requests.get("https://lar.inf.ufes.br/aula/getHorarioReservas?labgrad_id=3&data_atual=2022-05-05")
+    return req.json()
 
 # Coloca o site no ar
 if __name__ == '__main__':
