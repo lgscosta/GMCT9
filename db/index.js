7d8cd9522aa9ceb917f0f544b83476457721c246
ezschneider <ezequielschneiderr@gmail.com>
2022-08-09 22:20:24 -0300

    login, registration, homepage, ru, curriculum progression

--- db/index.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-exports.initDataBase = async () => {
-    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@exampleback.yppqj.mongodb.net/?retryWrites=true&w=majority`);
-}
--- models/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = require('./users')
-
-// Creating model objects
-const User = mongoose.model('User', userSchema);
-
- module.exports = {User}
